a1ab47c0104363874f2172020b4d40caf85e9aa8
RajendraGund <rajendragund2015@gmail.com>
2021-09-07 21:55:33 +0530

    Added : Program for To find Maximum number

---
+++ src/main/java/com/bridgelabz/javagenerics/Maximum.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.javagenerics;
+
+ 
+public class Maximum 
+{ 
+	public static Integer getmaximum(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
+			max = b;
+		else if (c.compareTo(max) > 0)
+			max = c;
+		// System.out.println(max);
+		return max;
+
+	}
+}
---
+++ src/test/java/com/bridgelabz/javagenerics/MaximumTest.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.javagenerics;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MaximumTest {
+	@Test
+	public void checkmaximum1() {
+		int max = Maximum.getmaximum(9, 4, 1);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum2() {
+		int max = Maximum.getmaximum(1, 9, 4);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum3() {
+		int max = Maximum.getmaximum(1, 4, 9);
+		Assert.assertEquals(9, max);
+	}
+
+}
